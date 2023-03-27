2c677b963a45aecfe0ba7692de5a00b110fda1d3
Atul <atul43653@gmail.com>
2023-03-27 17:05:23 +0530

    equating 2 lengths

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,24 @@ public class Main {
         double y2 = Sc.nextDouble();
         double length = Math.sqrt(x2-x1) +Math.sqrt(y2-y1);
         System.out.println("Lenght between two points = " +length);
+        // equating 2 line's length
+        System.out.println("enter the a1 = ");
+        double a1 = Sc.nextDouble();
+        System.out.println("enter the b1 = ");
+        double b1 = Sc.nextDouble();
+        System.out.println("enter the a2 = ");
+        double a2 = Sc.nextDouble();
+        System.out.println("enter the b2 = ");
+        double b2 = Sc.nextDouble();
+        double length1 = Math.sqrt((a2-a1) + (b2-b1));
+        System.out.println("length1 = " +length1);
+        if ( length==length1){
+            System.out.println("length are equal");
+        }else{
+            System.out.println("length are not equal");
+        }
     }
 }
+
+
+
