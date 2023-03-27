1362365fc62a84f3dc427a38999efb412c6d3787
Atul <atul43653@gmail.com>
2023-03-27 17:00:23 +0530

    length between 2 points

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to line Comparison");
+        // length between 2 coordinates
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("enter the x1 = ");
+        double x1 = Sc.nextDouble();
+        System.out.println("enter the y1 = ");
+        double y1 = Sc.nextDouble();
+        System.out.println("enter the x2 = ");
+        double x2 = Sc.nextDouble();
+        System.out.println("enter the y2 = ");
+        double y2 = Sc.nextDouble();
+        double length = Math.sqrt(x2-x1) +Math.sqrt(y2-y1);
+        System.out.println("Lenght between two points = " +length);
     }
 }
